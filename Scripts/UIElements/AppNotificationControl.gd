21f81ddce1d3c373148f3b1cdccc07b33c2071f2
Z1HEd <ziihed@gmail.com>
2025-12-17 14:30:27 +0100

    tidy up AppNotification

--- Scripts/UIElements/AppNotificationControl.gd
+++ Scripts/UIElements/AppNotificationControl.gd
@@ -4,11 +4,11 @@ class_name AppNotificationControl
 @onready var _label = %Label
 @onready var _progress = %ProgressBar
 
-func start(msg: String, dur := 5.0) -> void:
+func start(msg: String, duration := 5.0) -> void:
 	_label.text = msg
 	
-	_progress.max_value = dur
-	_progress.value = dur
+	_progress.max_value = duration
+	_progress.value = duration
 	
 	process_mode = Node.PROCESS_MODE_INHERIT
 
